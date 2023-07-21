17b653d0d4734382d598b0165e3b2931b5aad860
Karen <karen.oliveira@estudante.ufjf.br>
2023-07-21 15:08:32 -0300

    Pilha para operar árvore

--- Teste.cpp
+++ Teste.cpp
@@ -1,5 +1,4 @@
 #include "NoArv.h"
-
 #include <iostream>
 #include <math.h>
 #include <utility>
@@ -48,25 +47,42 @@ void leArquivo(int *x,int *y,int *z,int *esperado){
     return;
 }
 
+
+
+
 int main(){
 
     Arv arv;
 
     char operadores[4] = {'+', '-', '*', '/'};
     char variaveis[3] = {'x', 'y', 'z'};
-    arv.implementa(operadores,variaveis);
+    int size_op = sizeof(operadores)/sizeof(operadores[0]);
+    int size_var = sizeof(variaveis)/sizeof(variaveis[0]);
 
+    srand(98);
+    arv.implementa(arv.raiz,0,operadores,variaveis,size_op,size_var);
+    
+    Pilha p;
+    arv.empilha_arv(arv.raiz,&p);
+    
+    Pilha p2;
+    p.copia_pilha(&p2);
+    
     int x[10];
     int y[10];
     int z[10];
     int valor_esperado[10];
     
     arv.imprime();
+    cout << endl;
     leArquivo(x,y,z,valor_esperado);
-
     cout << "x\t" << "y\t" << "z\t" << "Valor esperado" << endl;
     for(int i=0;i<10;i++){
         cout << x[i]<< "\t" << y[i] << "\t" << z[i] << "\t" << valor_esperado[i] << endl;
     }
+
+    p.imprime_pilha();
+    p2.imprime_pilha();
+
     return 0;
 }
