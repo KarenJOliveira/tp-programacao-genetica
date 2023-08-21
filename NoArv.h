a67f9f9aa6007e3a8868a0fee642b7a724b9b133
Karen <karen.oliveira@estudante.ufjf.br>
2023-08-20 21:21:33 -0300

    teste criação novas árvores

--- NoArv.h
+++ NoArv.h
@@ -1,3 +1,10 @@
+//
+// Created by karen on 17/08/2023.
+//
+
+#ifndef UNTITLED_NOARV_H
+#define UNTITLED_NOARV_H
+
 #include "Pilha.h"
 #include <iostream>
 #include <random>
@@ -7,7 +14,7 @@ using namespace std;
 
 typedef struct NoArv
 {
-    public:
+public:
     char info;
     int idx;
     NoArv *filho_esquerda;
@@ -37,26 +44,31 @@ typedef struct Arv
         cont = 0;
         altura_max = max_alt;
     }
-    
+
     NoArv* aloca_no();
     void libera(NoArv *no);
     void imprime();
     void auxImprime(NoArv *no);
-    
+
     void implementa(NoArv *no,int altura, char *operadores, char *variaveis, int size_op, int size_var);
     void empilha_arv(NoArv *no,Pilha *p);
-    
+
     int contaNos(NoArv *no);
-    void muta_arvore(NoArv *no, Arv *sub_arv, int idx);
+
+    void remove(NoArv *sub_raiz, int val);
+    NoArv* auxRemove(NoArv *no_atual, NoArv *novo, int idx);
+    void muta_arvore(Arv *sub_arv);
+
+    void recombina_arvore(Arv *nova);
 }Arv;
 
 
 NoArv* Arv::aloca_no()
 {
-    if(this->cont < MAX){
-        this->cont++;
-        nos[this->cont].idx = this->cont;
-        return &nos[this->cont];
+    if(cont < MAX){
+        cont++;
+        nos[cont].idx = cont;
+        return &nos[cont];
     }
     return NULL;
 }
@@ -80,16 +92,17 @@ void Arv::libera(NoArv *no)
         libera(no->filho_esquerda);
         libera(no->filho_direita);
         delete no;
+        cont--;
     }
 }
 
 void Arv::implementa(NoArv *no,int altura, char *operadores, char *variaveis, int size_op, int size_var)
 {
-    if(altura == this->altura_max){
-        
+    if(altura == altura_max){
+
         no->info = operadores[rand()%size_op];
         no->ehOperador = true;
-        
+
         no->filho_esquerda = aloca_no();
         no->filho_direita = aloca_no();
         no->filho_esquerda->info = variaveis[rand()%size_var];
@@ -140,8 +153,61 @@ int Arv::contaNos(NoArv *no)
         return 0;
 }
 
-void Arv::muta_arvore(NoArv *no, Arv *sub_arv, int idx)
+
+void Arv::remove(NoArv *sub_raiz, int idx)
 {
-    int aux = contaNos(no);
-    int aux2 = contaNos(sub_arv->raiz);
+    raiz = auxRemove(raiz, sub_raiz, idx);
 }
+
+NoArv* Arv::auxRemove(NoArv *no_atual, NoArv *novo, int idx)
+{
+    if(no_atual->filho_esquerda == NULL || no_atual->filho_esquerda == NULL)
+    {
+        return NULL;
+    }
+    else if(no_atual->filho_esquerda->idx == idx){
+        NoArv *aux = no_atual->filho_esquerda;
+        cout << "Sub-arvore removida: " << endl;
+        auxImprime(aux);
+        cout << endl;
+        //libera(aux);
+        no_atual->filho_esquerda = novo;
+        return no_atual;
+    }else if(no_atual->filho_direita->idx == idx) {
+        NoArv *aux = no_atual->filho_direita;
+        cout << "Sub-arvore removida: " << endl;
+        auxImprime(aux);
+        cout << endl;
+
+        //libera(aux);
+        no_atual->filho_direita = novo;
+
+        return no_atual;
+    }
+
+    no_atual->filho_esquerda = auxRemove(no_atual->filho_esquerda,novo,idx);
+    no_atual->filho_direita = auxRemove(no_atual->filho_direita,novo,idx);
+    return no_atual;
+}
+
+
+void Arv::muta_arvore(Arv *sub_arv)
+{
+    int rand_ = rand()%(cont-1);
+    remove(sub_arv->raiz, rand_);
+    cont = contaNos(raiz);
+}
+
+void Arv::recombina_arvore(Arv *nova){
+    //selecionar uma sub-árvore da árvore principal
+    int rand_ = rand()%(cont-1);
+
+    //selecionar uma sub-árvore da nova árvore
+    int rand_ = rand()%(nova->cont-1);
+    //guardar ponteiro para ambas
+    //inserir sub-árvore da nova no local da sub-árvore da principal
+    //inserir sub-árvore da principal no local da sub-árvore da nova
+}
+
+
+#endif //UNTITLED_NOARV_H
